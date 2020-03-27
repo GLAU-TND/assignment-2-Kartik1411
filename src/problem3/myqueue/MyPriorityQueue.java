666e9351f4f24bf153ddace78e8b92f2be6212eb
Kartik1411 <kartikvarshney1411@gmail.com>
2020-03-27 13:01:24 +0530

    Create TreeNode class. Create private fields and make getter and setter.

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -1,10 +1,53 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:15 PM
- */
 package problem3.myqueue;
 
-public class MyPriorityQueue {
+
+import problem3.node.Node;
+
+public class MyPriorityQueue
+{
+    private Node temp, front, end;
+    private  int i = 0;
+
+
+    // inserting  new student into into queue
+    // and priority is set by roll number
+    public void enqueue(Node newNode)
+    {
+
+        if (front == null || newNode.getS().getRollNo() < front.getS().getRollNo())
+        {
+            newNode.setNext(front);
+            front = newNode;
+        }
+        else
+            {
+            temp = front;
+
+            while (temp.getNext() != null && temp.getNext().getS().getRollNo() < newNode.getS().getRollNo())
+            {
+                temp = temp.getNext();
+            }
+            newNode.setNext(temp.getNext());
+            temp.setNext(newNode);
+            }
+    }
+
+    // printing queue
+
+    public void dequeue()
+    {
+        if (front == null)
+        {
+            System.out.println("No entry found");
+            return;
+        }
+        do
+            {
+            System.out.println(i++);
+            System.out.print(front.getS().getName() + ":");
+            System.out.println(front.getS().getRollNo());
+            front = front.getNext();
+        }
+        while (front != null);
+    }
 }
--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -1,10 +1,110 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
+
 package problem5.circularqueue;
+
+import problem5.node.Node;
+
+
 //to implement circular queue
-public class MyCircularQueue {
+public class MyCircularQueue
+{
+    private Node front, temp, end;
+
+    public MyCircularQueue()
+    {
+        front = null;
+        temp = null;
+        end = null;
+    }
+
+
+    public Node getFront()
+    {
+        return front;
+    }
+
+    public void setFront(Node front)
+    {
+        this.front = front;
+    }
+
+    public Node getTemp()
+    {
+        return temp;
+    }
+
+    public void setTemp(Node tmp)
+    {
+        this.temp = tmp;
+    }
+
+    public void enqueue(Node newNode)
+    {
+        if (front == null)
+        {
+            temp = front = newNode;
+            return;
+        }
+        if (temp.getNext() == null)
+        {
+            temp.setNext(newNode);
+            newNode.setNext(temp);
+            end = newNode;
+            return;
+        }
+        newNode.setNext(temp.getNext());
+        temp.setNext(newNode);
+
+    }
+
+    public void printQueue()
+    {
+        temp = front;
+        try
+        {
+            do
+                {
+                System.out.println(temp.getS().toString());
+                temp = temp.getNext();
+                }
+            while (temp != front && temp != null);
+        } catch (NullPointerException ignored) {}
+    }
+
+
+    public void remove(String name)
+    {
+        temp = front;
+        if (temp.getS().getName().equals(name) && temp.getS().getBacklog() == 0)
+        {
+            temp = front = front.getNext();
+        }
+        while (!temp.getNext().getS().getName().equals(name))
+        {
+            temp = temp.getNext();
+            if (temp == front)
+                return;
+        }
+        if (temp.getS().getBacklog() == 0)
+        {
+            temp.setNext(temp.getNext().getNext());
+        }
+    }
+
+    public void process(String name)
+    {
+        temp = front;
+        if (temp.getS().getName().equals(name))
+        {
+            System.out.println(temp.getS().toString());
+            System.out.println(temp.getS().getBacklog() - temp.getS().getAppearingCount());
+        }
+        while(!temp.getS().getName().equals(name))
+        {
+            temp = temp.getNext();
+            if(temp == front)
+                return;
+        }
+        System.out.println(temp.getS().toString());
+        System.out.println(temp.getS().getBacklog() - temp.getS().getAppearingCount());
+    }
 }
